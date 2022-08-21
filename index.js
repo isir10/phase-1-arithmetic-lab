6ba05ee0bb8378042943e2b50de580a917ccc55b
isir10 <isir.malak@moringaschool.com>
2022-08-21 23:29:22 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 =31;
+let num2 =2;
+const multiply = num1*num2;
+let random = Math.floor(Math.random() * 150) + 1;
+let num3= 24;
+let num4 =5;
+let mod = num3%num4;
+let max = Math.max(8, 3, 20, 7, 6, 5, 18);
